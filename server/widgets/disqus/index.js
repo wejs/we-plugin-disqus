acf67fb5feb594e6c138e33499f80300acd2e8b8
albertosouza <alberto.souza.99@gmail.com>
2016-02-11 22:17:51 -0200

    update disqus code, add widget locales and update widget

--- server/widgets/disqus/index.js
+++ server/widgets/disqus/index.js
@@ -7,18 +7,12 @@
 module.exports = function disqusWidget(projectPath, Widget) {
   var widget = new Widget('disqus', __dirname);
 
-  // Override default widget class functions after instance
-  //
-  widget.afterSave = function disqusWidgetAfterSave(req, res, next) {
+  widget.beforeSave = function disqusWidgetBeforeSave(req, res, next) {
     req.body.configuration = {
       disqus_shortname: req.body.disqus_shortname
     };
     return next();
   };
-
-  // widget.formMiddleware = function formMiddleware(req, res, next) {
-  //   next();
-  // }
 
   return widget;
 };
